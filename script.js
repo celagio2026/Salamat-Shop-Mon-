bb914d67060c57231c4690805869b5a3db330935
celagio2026 <117282010+celagio2026@users.noreply.github.com>
2023-05-14 22:12:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ accordion.forEach(acco => {
 });
 
 /* menu */
-var a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0, k = 0, l = 0, prod_name = 0, prod_price =0, totalprod_price = 0, cust_order = [];
+var a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0, p = 0, q = 0, r = 0, s = 0, t = 0, u = 0, v = 0, w = 0, x = 0, prod_name = 0, prod_price =0, totalprod_price = 0, cust_order = [];
 function formSubmit() {
     while (a > 0) {
         var cust_order1 = {
